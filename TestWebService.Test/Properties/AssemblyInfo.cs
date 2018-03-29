39117901c785c2776c179ec131f28d90eae88484
rashidali1604 <i141604@nu.edu.pk>
2018-03-30 02:39:25 +0500

    initial commit

---
+++ TestWebService.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestWebService.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestWebService.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10e60b0e-34a6-4987-92c5-80dc6872fbe6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestWebService.Test/WebServiceTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebService;
+using System.Collections.Generic;
+using ProcesssAutomation;
+
+namespace TestWebService.Test
+{
+    [TestClass]
+    public class WebServiceTest
+    {
+        [TestMethod]
+        public void TestCreateTable()
+        {
+
+            WebService1 service = new WebService1();
+            String TableName = "Student", ColumnName = "RollNo,Name,Age";
+            int res = service.CreateTable(TableName, ColumnName);
+            Assert.AreNotEqual(0, res);
+
+        }
+
+        [TestMethod]
+        public void TestenterData()
+        {
+            WebService1 service = new WebService1();
+            String TableName="Student", values = "i141604,Rashid Ali,23", columns = "RollNo,Name,Age";
+            int res = service.enterData(TableName,columns, values);
+            Assert.AreEqual(1, res);
+            TableName = "Test";
+             res = service.enterData(TableName, columns, values);
+            Assert.AreNotEqual(1, res);
+
+        }
+
+        [TestMethod]
+        public void TestreturnData()
+        {
+            WebService1 service = new WebService1();
+            String TableName = "Student";
+            String[] res = service.returnData(TableName);
+            Assert.AreNotEqual(null, res);
+            TableName = "Test";
+            res = service.returnData(TableName);
+            Assert.AreEqual(null, res);
+        }
+
+        [TestMethod]
+        public void TestgetTableData()
+        {
+            WebService1 service = new WebService1();
+            String TableName = "Student";
+            List<GroupMembers> res = service.getTableData(TableName);
+            Assert.AreNotEqual(null, res);
+            TableName = "Test";
+            res = service.getTableData(TableName);
+            Assert.AreEqual(null, res);
+        }
+
+        [TestMethod]
+        public void TestLogin()
+        {
+            WebService1 service = new WebService1();
+            String TableName = "UsersTable";
+            String email = "rashid@gmail.com", password = "1234";
+            String res = service.Login(email, password, TableName);
+            Assert.AreNotEqual(null, res);
+            Assert.AreEqual("true,Student", res.Trim());
+            email = "asim@yahoo.com"; password = "1234";
+            res = service.Login(email, password, TableName);
+            Assert.AreEqual("true,Instructor", res.Trim());
+            email = "asim@yahoo.com"; password = "1234";TableName = "Test";
+            res = service.Login(email, password, TableName);
+            Assert.AreEqual(null, res);
+
+
+        }
+
+
+    }
+}
